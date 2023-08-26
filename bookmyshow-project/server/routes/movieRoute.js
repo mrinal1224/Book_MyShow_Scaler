0f648560e1d83bfd0e3fc2f8e2a0f14f23067fac
mrinal1224 <iammrinalkc@gmail.com>
2023-08-26 23:34:25 +0530

    movie Api

---
+++ bookmyshow-project/server/routes/movieRoute.js
@@ -0,0 +1,25 @@
+const router = require('express').Router()
+const Movie = require('../models/movieModel')
+
+
+// Add a new Movie
+
+router.post('/add-movie' , async (req , res)=>{
+    try {
+        const newMovie = new Movie(req.body)
+        await newMovie.save()
+        res.send({
+            success: true,
+            message: "Movie added successfully",
+          });
+    } catch (error) {
+        res.send({
+            success: false,
+            message: error.message,
+          });
+    }
+})
+
+module.exports = router;
+
+
--- bookmyshow-project/server/server.js
+++ bookmyshow-project/server/server.js
@@ -7,9 +7,11 @@ app.use(cors());
 require('dotenv').config()
 const dbConfig = require('./config/dbConfig')
 const userRoute = require('./routes/userRoute')
+const movieRoute = require('./routes/movieRoute')
 
 app.use(express.json())
 app.use('/api/users' , userRoute)
+app.use('/api/users' , movieRoute)
 
 
 

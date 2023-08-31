ac005947d59f50d49149f6d7d64c4fc99ed9fa5f
mrinal1224 <iammrinalkc@gmail.com>
2023-08-31 22:43:23 +0530

    Modal for shows

--- bookmyshow-project/client/src/pages/Profile/Shows/index.js
+++ bookmyshow-project/client/src/pages/Profile/Shows/index.js
@@ -1,7 +1,69 @@
-import { Modal } from 'antd'
-import React from 'react'
+import { Col, Form, Modal, Row, Table, message } from "antd"
+import React , {useState} from 'react'
+import Button from '../../../components/Button'
 
 function Shows({ openShowsModal, setOpenShowsModal, theatre }) {
+
+    let [view, setView] = useState("table");
+
+    const columns = [
+        {
+          title: "Show Name",
+          dataIndex: "name",
+        },
+        {
+          title: "Date",
+          dataIndex: "date",
+          render: (text, record) => {
+            // return moment(text).format("MMM Do YYYY");
+          },
+        },
+        {
+          title: "Time",
+          dataIndex: "time",
+        },
+        {
+          title: "Movie",
+          dataIndex: "movie",
+          render: (text, record) => {
+            return record.movie.title;
+          },
+        },
+        {
+          title: "Ticket Price",
+          dataIndex: "ticketPrice",
+        },
+        {
+          title: "Total Seats",
+          dataIndex: "totalSeats",
+        },
+        {
+          title: "Available Seats",
+          dataIndex: "availableSeats",
+          render: (text, record) => {
+            return record.totalSeats - record.bookedSeats.length;
+          },
+        },
+        {
+          title: "Action",
+          dataIndex: "action",
+          render: (text, record) => {
+            return (
+              <div className="flex gap-1 items-center">
+                {record.bookedSeats.length === 0 && (
+                  <i
+                    className="ri-delete-bin-line"
+                    // onClick={() => {
+                    //   handleDelete(record._id);
+                    // }}
+                  ></i>
+                )}
+              </div>
+            );
+          },
+        },
+      ];
+
     return (
         <Modal title=""
             open={openShowsModal}
@@ -12,7 +74,116 @@ function Shows({ openShowsModal, setOpenShowsModal, theatre }) {
             <h1 className="text-primary text-md uppercase mb-1">
                 Theatre : {theatre.name}
             </h1>
-        </Modal>
+
+            <hr/>
+
+    <div className="flex justify-between mt-1 mb-1 items-center">
+        <h1 className="text-md uppercase">
+          {view === "table" ? "Shows" : "Add Show"}
+        </h1>
+        {(
+          <Button
+            variant="outlined"
+            title="Add Show"
+            onClick={() => {
+              setView("form");
+            }}
+          />
+        )}
+      </div>
+
+
+      {view==='table' && <Table columns={columns}/>}
+
+
+      {view === "form" && (
+        <Form layout="vertical">
+          <Row gutter={[16, 16]}>
+            <Col span={8}>
+              <Form.Item
+                label="Show Name"
+                name="name"
+                rules={[{ required: true, message: "Please input show name!" }]}
+              >
+                <input />
+              </Form.Item>
+            </Col>
+            <Col span={8}>
+              <Form.Item
+                label="Date"
+                name="date"
+                rules={[{ required: true, message: "Please input show date!" }]}
+              >
+                <input
+                  type="date"
+                  min={new Date()}
+                />
+              </Form.Item>
+            </Col>
+
+            <Col span={8}>
+              <Form.Item
+                label="Time"
+                name="time"
+                rules={[{ required: true, message: "Please input show time!" }]}
+              >
+                <input type="time" />
+              </Form.Item>
+            </Col>
+
+            <Col span={8}>
+              <Form.Item
+                label="Movie"
+                name="movie"
+                rules={[{ required: true, message: "Please select movie!" }]}
+              >
+                <select>
+                  <option value="">Select Movie</option>
+                  {/* {movies.map((movie) => (
+                    <option value={movie._id}>{movie.title}</option>
+                  ))} */}
+                </select>
+              </Form.Item>
+            </Col>
+
+            <Col span={8}>
+              <Form.Item
+                label="Ticket Price"
+                name="ticketPrice"
+                rules={[
+                  { required: true, message: "Please input ticket price!" },
+                ]}
+              >
+                <input type="number" />
+              </Form.Item>
+            </Col>
+
+            <Col span={8}>
+              <Form.Item
+                label="Total Seats"
+                name="totalSeats"
+                rules={[
+                  { required: true, message: "Please input total seats!" },
+                ]}
+              >
+                <input type="number" />
+              </Form.Item>
+            </Col>
+          </Row>
+
+          <div className="flex justify-end gap-1">
+            <Button
+              variant="outlined"
+              title="Cancel"
+              onClick={() => {
+                setView("table");
+              }}
+            />
+            <Button variant="contained" title="SAVE" type="submit" />
+          </div>
+        </Form>
+      )}
+    </Modal>
     )
 }
 

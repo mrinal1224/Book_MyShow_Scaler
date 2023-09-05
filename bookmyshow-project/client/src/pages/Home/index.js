743d2e404686876c2014b820c0d403683c1d3b83
mrinal1224 <iammrinalkc@gmail.com>
2023-09-05 20:58:22 +0530

    upcoming

--- bookmyshow-project/client/src/pages/Home/index.js
+++ bookmyshow-project/client/src/pages/Home/index.js
@@ -1,24 +1,18 @@
-import React , {useEffect, useState} from 'react'
-
-import {Row , Col , message} from 'antd'
-import { GetAllMovies } from '../../apicalls/movies';
+import React, { useEffect } from "react";
+import { Col, message, Row, Input} from "antd";
 import { useDispatch } from "react-redux";
 import { HideLoading, ShowLoading } from "../../redux/loadersSlice";
+import { GetAllMovies } from "../../apicalls/movies";
+import { useNavigate } from "react-router-dom";
+import moment from "moment";
+import { GetAllUpcomingMovies } from "../../apicalls/upcoming";
 
-import moment from 'moment'
-
-import {useNavigate} from 'react-router-dom'
-
-
-
-
-const Home = () => {
-
-  const [searchText, setSearchText] = React.useState("");
+function Home() {
+  const [searchText = "", setSearchText] = React.useState("");
   const [movies, setMovies] = React.useState([]);
-
+  const [upcoming, setUpcoming] = React.useState([]);
+  const navigate = useNavigate();
   const dispatch = useDispatch();
-  const navigate = useNavigate()
   const getData = async () => {
     try {
       dispatch(ShowLoading());
@@ -33,29 +27,54 @@ const Home = () => {
       dispatch(HideLoading());
       message.error(error.message);
     }
-  }
+  };
+
+  const getUpcomingMoviesData = async () => {
+    try {
+      dispatch(ShowLoading());
+      const response = await GetAllUpcomingMovies();
+      if (response.success) {
+        setUpcoming(response.data);
+      } else {
+        message.error(response.message);
+      }
+      dispatch(HideLoading());
+    } catch (error) {
+      dispatch(HideLoading());
+      message.error(error.message);
+    }
+  };
 
 
-  useEffect(()=>{
-    getData()
-  } , [])
+  useEffect(() => {
+    getData();
+  }, []);
 
-
-
-
-
-
-
+  useEffect(() => {
+    getUpcomingMoviesData();
+  }, []);
   return (
     <div>
-     <input type='text' placeholder='Search For Currenty Showing Movies' className='search-input' onChange={(e)=> setSearchText(e.target.value)}/>
-     <Row gutter={[20]} className="mt-2">
+    <div>
+      <Input
+        style={{ width: "600px" }}
+        type="text"
+        className="search-input"
+        placeholder="Search for movies"
+        value={searchText}
+        onChange={(e) => setSearchText(e.target.value)}
+      />
+
+ 
+      <h1 className="text-md uppercase mb-2">Currently Showing Movies</h1>
+
+      <Row gutter={[20]} className="mt-2">
         {movies
         .filter((movie) => movie.title.toLowerCase().includes(searchText.toLowerCase()))
         .map((movie) => (
           <Col span={6}>
             <div
-              className="card flex flex-col gap-1 cursor-pointer"
+              className="card flex flex-col gap-3 cursor-pointer"
               onClick={() =>
                 navigate(
                   `/movie/${movie._id}?date=${moment().format("YYYY-MM-DD")}`
@@ -72,9 +91,36 @@ const Home = () => {
         ))}
       </Row>
 
-       
+
+
     </div>
-  )
+
+    <h1 className="text-md uppercase mb-2">Upcoming Movies</h1>
+
+    <Row gutter={[20]} className="mt-2">
+        {upcoming
+        .map((movie) => (
+          <Col span={6}>
+            <div
+              className="card flex flex-col gap-2 cursor-pointer"
+              onClick={() =>
+                navigate(
+                  `/movie/${movie._id}?date=${moment().format("YYYY-MM-DD")}`
+                )
+              }
+            >
+              <img src={movie.poster} alt="" height={200} />
+
+              <div className="flex justify-center p-1">
+                <h1 className="text-md uppercase">{movie.title}</h1>
+              </div>
+            </div>
+          </Col>
+        ))}
+      </Row>
+    
+    </div>
+  );
 }
 
-export default Home
+export default Home; 
